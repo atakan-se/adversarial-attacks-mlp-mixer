fe7303479f264cf49c7cbcf3446bf07f2c14743b
atakan-se <71789136+atakan-se@users.noreply.github.com>
2021-10-22 00:24:28 +0300

    finished mlp-mixer

---
+++ mlp-mixer.py
@@ -0,0 +1,61 @@
+import torch
+from torch import nn
+
+class MLP(nn.Module):
+    def __init__(self, input_dims, hidden_dims, out_dims):
+        super().__init__()
+        self.fc1 = nn.Linear(input_dims, hidden_dims)
+        self.fc2 = nn.Linear(hidden_dims, out_dims)
+        self.activation = nn.GELU()
+    
+    def forward(self, x):
+        return self.fc2(self.activation(self.fc1(x)))
+
+class Mixer(nn.Module):
+    def __init__(self, num_patches, input_channels, token_mlp_dim, channel_mlp_dim):
+        super().__init__()
+        self.token_mlp = MLP(num_patches, token_mlp_dim, num_patches)
+        self.channel_mlp = MLP(input_channels, channel_mlp_dim, input_channels)
+        self.layernorm1 = nn.LayerNorm(input_channels, eps=1e-6)
+        self.layernorm2 = nn.LayerNorm(input_channels, eps=1e-6)
+
+    def forward(self, x):
+        """
+        x = (BatchSize, PatchCount, Channels)
+        """
+        y = self.layernorm1(x)
+        y = torch.transpose(y, 1, 2)
+        y = self.token_mlp(y)
+        y = torch.transpose(y, 1, 2)
+        z = y + x
+        y = self.layernorm2(z)
+        return  z + self.channel_mlp(y)
+
+class MLPMixer(nn.Module):
+    def __init__(self, input_dims, # (C,H,W)
+                       patch_size, # Int or Tuple 
+                       num_classes, # Target classes
+                       num_blocks, # Mixer blocks
+                       hidden_dims, # How many channels each patch should be mapped at stem
+                       token_mlp_dim, # Hidden dimensions for token MLP
+                       channel_mlp_dim): # Hidden dimensions for channel MLP
+        super().__init__()
+        C, H, W = input_dims
+        assert (H*W) % (patch_size**2) == 0 # Make sure equal patch sizes are possible
+        self.num_patches = (H*W) // (patch_size**2)
+        self.stem = nn.Conv2d(C, hidden_dims, kernel_size=patch_size, stride=patch_size)
+        self.mixers = nn.Sequential(*[Mixer(self.num_patches, hidden_dims, token_mlp_dim, channel_mlp_dim) for _ in range(num_blocks)])
+        self.layernorm = nn.LayerNorm(hidden_dims, eps=1e-6)
+        self.classifier = nn.Linear(hidden_dims, num_classes)
+        nn.init.zeros_(self.classifier.weight) 
+    
+    def forward(self, x):
+        y = self.stem(x) # y is now N C H W
+        y = torch.flatten(y, start_dim=2, end_dim=3) # y is now N C P
+        y = torch.transpose(y, 1, 2) # y is now N P C
+        for mixer_block in self.mixers:
+            y = mixer_block(y) 
+        y = self.layernorm(y)
+        y = torch.mean(y, dim=1, keepdim=False)
+        y = self.classifier(y)
+        return y
